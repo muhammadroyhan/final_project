8aec7d8038a23fef05962d746eb95b1307ffce7d
Muhammad Rayhan Messala Ahsan <rayhanahsan14@gmail.com>
2022-12-05 15:22:43 +0700

    update

--- lib/views/add_task.dart
+++ lib/views/add_task.dart
@@ -5,8 +5,29 @@ class addTask extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController textEditingController = TextEditingController();
+
     return Scaffold(
-      body: ,
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                hintText: "Title",
+              ),
+            ),
+            TextFormField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                hintText: "Description",
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
