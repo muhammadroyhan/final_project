45db81f70318b76988e49a183e5aeca7a12e9db4
Muhammad Rayhan Messala Ahsan <rayhanahsan14@gmail.com>
2022-12-06 10:56:36 +0700

    update

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -129,7 +129,7 @@ class _HomePageState extends State<HomePage>
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => addTask(),
+                builder: (context) => AddNewTask(),
               ),
             );
           },
